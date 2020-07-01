514a08078cf5dc22a2f11179f97bc69732db0094
Adeola Adeyemo <adeola@formpl.us>
2020-07-01 16:30:35 +0100

    PORT-5 | Fixed issue with footer
    Signed-off-by: Adeola Adeyemo <adeola@formpl.us>

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
-import './footer.css'
+import { css } from '@emotion/core'
 
 const Footer = () => (
-  <footer className={'footer'}>
+  <footer className={'footer'} css={css`
+    padding: 30px 0;
+    text-align: center;
+    width: 100%;
+  `}>
     © {new Date().getFullYear()} Adeola J. Adeyemo
   </footer>
 )
